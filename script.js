8a9672b1c5374b6c00930dce0bef3e692c9dc56b
Fábio Vicente <fab10_lima@hotmail.com>
2021-12-01 05:53:10 -0300

    Requisito 5: Desenvolvendo a dinâmica do jogo

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const tries = document.getElementById('tries');
+const colors = tries.children;
 const rightColor = document.getElementById('rgb-color');
+const rightAnswer = parseInt(Math.random() * 6, 10);
+const answer = document.getElementById('answer');
 
 function generateColors() {
-  const colors = tries.children;
-  const rightAnswer = parseInt(Math.random() * 6, 10);
   for (let i = 0; i < colors.length; i += 1) {
     const R = parseInt(Math.random() * 256, 10);
     const G = parseInt(Math.random() * 256, 10);
     const B = parseInt(Math.random() * 256, 10);
     colors[i].style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
   }
-  colors[rightAnswer].style.backgroundColor = `rgb(${rightColor.innerText})`;
+  colors[rightAnswer].style.backgroundColor = `rgb${rightColor.innerText}`;
+}
+function verifyColor(click) {
+  const color = click.target;
+  if (color !== tries) {
+    if (color === colors[rightAnswer]) {
+      answer.innerText = 'Acertou!';
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
+    }
+  }
 }
 
 window.onload = generateColors;
+tries.onclick = verifyColor;
