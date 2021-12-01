c12d7d26ad6f73fd0139abd25d0e9f08db4bf1b5
Fábio Vicente <fab10_lima@hotmail.com>
2021-12-01 06:14:49 -0300

    Requisito 6: Reiniciando o jogo

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const tries = document.getElementById('tries');
 const colors = tries.children;
 const rightColor = document.getElementById('rgb-color');
-const rightAnswer = parseInt(Math.random() * 6, 10);
 const answer = document.getElementById('answer');
+const reset = document.getElementById('reset-game');
 
+function initAnswer() {
+  return parseInt(Math.random() * 6, 10);
+}
+
+let rightAnswer = initAnswer();
+
+function ramdomColor() {
+  const R = parseInt(Math.random() * 256, 10);
+  const G = parseInt(Math.random() * 256, 10);
+  const B = parseInt(Math.random() * 256, 10);
+  return `(${R}, ${G}, ${B})`;
+}
 function generateColors() {
   for (let i = 0; i < colors.length; i += 1) {
-    const R = parseInt(Math.random() * 256, 10);
-    const G = parseInt(Math.random() * 256, 10);
-    const B = parseInt(Math.random() * 256, 10);
-    colors[i].style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+    colors[i].style.backgroundColor = `rgb${ramdomColor()}`;
   }
   colors[rightAnswer].style.backgroundColor = `rgb${rightColor.innerText}`;
 }
@@ -23,6 +32,13 @@ function verifyColor(click) {
     }
   }
 }
+function resetGame() {
+  rightAnswer = initAnswer();
+  rightColor.innerText = ramdomColor();
+  answer.innerText = 'Escolha uma cor';
+  generateColors();
+}
 
 window.onload = generateColors;
 tries.onclick = verifyColor;
+reset.onclick = resetGame;
