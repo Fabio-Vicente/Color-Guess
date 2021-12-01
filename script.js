4dc0e69dbd68dc79dc70fea150f1f6b89786df05
Fábio Vicente <fab10_lima@hotmail.com>
2021-12-01 07:16:55 -0300

    Adicional: Criando um marcador de mais alta pontuação global

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const rightColor = document.getElementById('rgb-color');
 const answer = document.getElementById('answer');
 const reset = document.getElementById('reset-game');
 const score = document.getElementById('score');
+const highScore = document.getElementById('high-score');
+
+let alreadyPlayed = false;
+
 function initAnswer() {
   return parseInt(Math.random() * 6, 10);
 }
 let rightAnswer = initAnswer();
 
-/* if (sessionStorage.getItem('score') !== null) {
-  score.innerText = sessionStorage.getItem('score');
-} */
+if (localStorage.getItem('score') !== null) {
+  highScore.innerText = localStorage.getItem('score');
+}
 
 function ramdomColor() {
   const R = parseInt(Math.random() * 256, 10);
@@ -25,14 +29,26 @@ function generateColors() {
   }
   colors[rightAnswer].style.backgroundColor = `rgb${rightColor.innerText}`;
 }
-
+function beatHighScore() {
+  const numericScore = parseInt(score.innerText, 10);
+  const numericHighScore = parseInt(highScore.innerText, 10);
+  if (numericScore > numericHighScore) {
+    highScore.innerText = score.innerText;
+    localStorage.setItem('score', highScore.innerText);
+  }
+}
 function verifyColor(click) {
+  if (alreadyPlayed) {
+    return;
+  }
   const color = click.target;
   if (color !== tries) {
     if (color === colors[rightAnswer]) {
       answer.innerText = 'Acertou!';
       const numericScore = parseInt(score.innerText, 10) + 3;
       score.innerText = numericScore;
+      beatHighScore();
+      alreadyPlayed = true;
     } else {
       answer.innerText = 'Errou! Tente novamente!';
       score.innerText = 0;
@@ -44,7 +60,7 @@ function resetGame() {
   rightColor.innerText = ramdomColor();
   answer.innerText = 'Escolha uma cor';
   generateColors();
-  sessionStorage.setItem('score', score.innerText);
+  alreadyPlayed = false;
 }
 
 window.onload = generateColors;
