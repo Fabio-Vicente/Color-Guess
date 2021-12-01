53eb7c463141248a104f8735d43ec3f30716d805
Fábio Vicente <fab10_lima@hotmail.com>
2021-12-01 06:51:03 -0300

    Requisito 7: Criando um placar para contagem de pontos

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const colors = tries.children;
 const rightColor = document.getElementById('rgb-color');
 const answer = document.getElementById('answer');
 const reset = document.getElementById('reset-game');
-
+const score = document.getElementById('score');
 function initAnswer() {
   return parseInt(Math.random() * 6, 10);
 }
-
 let rightAnswer = initAnswer();
+
+/* if (sessionStorage.getItem('score') !== null) {
+  score.innerText = sessionStorage.getItem('score');
+} */
 
 function ramdomColor() {
   const R = parseInt(Math.random() * 256, 10);
@@ -22,13 +25,17 @@ function generateColors() {
   }
   colors[rightAnswer].style.backgroundColor = `rgb${rightColor.innerText}`;
 }
+
 function verifyColor(click) {
   const color = click.target;
   if (color !== tries) {
     if (color === colors[rightAnswer]) {
       answer.innerText = 'Acertou!';
+      const numericScore = parseInt(score.innerText, 10) + 3;
+      score.innerText = numericScore;
     } else {
       answer.innerText = 'Errou! Tente novamente!';
+      score.innerText = 0;
     }
   }
 }
@@ -37,6 +44,7 @@ function resetGame() {
   rightColor.innerText = ramdomColor();
   answer.innerText = 'Escolha uma cor';
   generateColors();
+  sessionStorage.setItem('score', score.innerText);
 }
 
 window.onload = generateColors;
